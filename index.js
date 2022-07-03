7662616f030bbf2b118acd11ec4c8ee35bf2ae95
shahram <shahramkhaleh2001@gmail.com>
2022-07-03 01:38:41 -0700

    End Project

--- index.js
+++ index.js
@@ -5,18 +5,55 @@ let allProduct = [];
 
 let filters = {
   filterItems: "",
+  filterButtons: "",
 };
 
 // DOM Elements
 const searchInput = document.querySelector("#search");
+const productsContainer = document.querySelector(".products-center");
+const buttons = document.querySelectorAll(".btn");
 
 // functions
+// Input Search Function
 function renderProducts(_prudocts, _filters) {
-  console.log(_prudocts);
   const filteredProducts = _prudocts.filter((product) =>
     product.title.toLowerCase().includes(_filters.filterItems)
   );
-  console.log(filteredProducts);
+  productsContainer.innerHTML = "";
+  filteredProducts.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList = "product";
+    productDiv.innerHTML = `
+   <div class="img-container">
+     <img src=${product.image} class="p-${product.class}" />
+   </div>
+   <div class="product-desc">
+     <p class="product-price">تومان ${product.price}</p>
+     <p class="product-title">${product.title}</p>
+   </div>`;
+    productsContainer.appendChild(productDiv);
+  });
+}
+
+// Button Search Function
+function renderByButton(allProduct, _filters) {
+  const filteredProducts = allProduct.filter((product) =>
+    product.class.toLowerCase().includes(_filters.filterButtons)
+  );
+  productsContainer.innerHTML = "";
+  filteredProducts.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList = "product";
+    productDiv.innerHTML = `
+  <div class="img-container">
+    <img src=${product.image} class="p-${product.class}" />
+  </div>
+  <div class="product-desc">
+    <p class="product-price">تومان ${product.price}</p>
+    <p class="product-title">${product.title}</p>
+  </div>`;
+    productsContainer.appendChild(productDiv);
+  });
 }
 
 // Evonts
@@ -24,10 +61,19 @@ document.addEventListener("DOMContentLoaded", () => {
   axios.get("http://localhost:3000/items").then((res) => {
     allProduct = res.data;
     renderProducts(allProduct, filters);
+    renderByButton(allProduct, filters);
   });
 });
 
 searchInput.addEventListener("input", (e) => {
   filters.filterItems = e.target.value;
   renderProducts(allProduct, filters);
+});
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const filterID = e.target.dataset.filter;
+    filters.filterButtons = filterID;
+    renderByButton(allProduct, filters);
+  });
 });
